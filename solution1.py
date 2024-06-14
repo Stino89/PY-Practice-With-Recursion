a8a60f722e2a071daaae8dc83ffa826f584d39d5
Faustino Ruiz <ftr1989@gmail.com>
2024-06-13 20:26:43 -0700

    solution1

--- solution1.py
+++ solution1.py
@@ -1,1 +1,10 @@
-# Write code for algorithm 1 below
+def recursive_count_down(n):
+	if n == 0:
+		print(n)
+		return
+
+	else:
+		print(n)
+		return recursive_count_down(n-1)
+
+recursive_count_down(7)
